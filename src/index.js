2e5db0f79c7bf6151ef2e3ff326894c11c0f6f22
TPSoares <thiagopina.soares@gmail.com>
2019-02-02 00:02:29 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import Reducers from "./reducers/index";
+import thunk from "redux-thunk";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const store = createStore(Reducers, applyMiddleware(thunk));
+store.subscribe(() => console.log("store", store.getState()));
+store.dispatch(getAllRestaurants());
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <Route exact path="/" component={App}></Route>
+        </Provider>
+    </BrowserRouter>, document.getElementById("root")
+);
