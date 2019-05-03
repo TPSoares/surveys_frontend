3253dceb0b5e4c95590b38a236bdc9f12fc0dfdf
TPSoares <thiagopina.soares@gmail.com>
2019-05-02 22:01:28 -0300

    feat: add color to survey based on its date

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -20,10 +20,44 @@ class Dashboard extends Component {
                 {
                     this.props.surveys.data &&
                     this.props.surveys.data.surveys.map(survey => {
+
+                        //check survey date
+                        var dateFrom = survey.start_date;
+                        var dateTo = survey.end_date;
+                        var dateCheck = new Date();
+
+                        var d1 = dateFrom.split("/");
+                        var d2 = dateTo.split("/");
+                        // var c = dateCheck.split("/");
+                        // console.log(d1);
+
+                        var from = new Date(d1[2], parseInt(d1[1])-1, d1[0]);  // -1 because months are from 0 to 11
+                        var to   = new Date(d2[2], parseInt(d2[1])-1, d2[0]);
+                        // var check = new Date(c[2], parseInt(c[1])-1, c[0]);
+
+                        var status = '';
+
+                        if(dateCheck > from && dateCheck < to) {
+                            status = 'between';
+                        }
+
+                        if(dateCheck > from && dateCheck > to) {
+                            status = 'over';
+                        }
+
+                        if(dateCheck < from && dateCheck < to) {
+                            status = 'coming';
+                        }
+                        console.log(dateCheck > from && dateCheck < to)
+                        console.log(from)
+                        console.log(status)
+
                         return (
 
                             <div key={survey.id} className="card surveys">
-                                <div className="card-header info"><strong>{survey.title}</strong> {survey.start_date} - {survey.end_date}</div>
+                                <div className="card-header info" style={{backgroundColor: status === 'between' ? '#1aff1a' : (status === 'coming' ? '#ffd633' : '#ff3333' )}}>
+                                    <strong>{survey.title}</strong> {survey.start_date} - {survey.end_date}
+                                </div>
 
                                 <div className="card-body">
                                     <p><strong>Description: </strong>{survey.description}</p>
